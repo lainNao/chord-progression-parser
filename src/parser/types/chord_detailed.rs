51ab8fab1035ae158d0bbdb8af5f7453fbe42a68
lainNao <k.yamada.biz.24@gmail.com>
2023-10-30 23:48:25 +0900

    仕様に合わせ修正

--- src/parser/types/chord_detailed.rs
+++ src/parser/types/chord_detailed.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 use super::{accidental::Accidental, base::Base, chord_type::ChordType, extension::Extension};
 use strum::VariantNames;
 
@@ -8,6 +10,13 @@ pub struct ChordDetailed {
     pub chord_type: ChordType,
     pub extension: Option<Extension>,
     pub additional_extension: Option<Extension>,
+}
+
+fn try_remove_prefix(input: &str, prefix: &str) -> String {
+    match input.strip_prefix(prefix) {
+        Some(stripped) => String::from(stripped),
+        None => String::from(input),
+    }
 }
 
 impl ChordDetailed {
@@ -38,46 +47,45 @@ impl ChordDetailed {
             _ => None,
         };
 
-        let chord_str = &s[idx..];
+        let chord_str_without_base = &s[idx..];
 
-        let chord_type = if chord_str.starts_with('m') {
+        let chord_type = if chord_str_without_base.starts_with('m') {
             ChordType::Minor
-        } else if chord_str.starts_with('M') {
+        } else if chord_str_without_base.starts_with('M') {
             ChordType::Major
-        } else if chord_str.starts_with("aug") {
+        } else if chord_str_without_base.starts_with("aug") {
             ChordType::Augmented
-        } else if chord_str.starts_with("dim") {
+        } else if chord_str_without_base.starts_with("dim") {
             ChordType::Diminished
         } else {
             ChordType::Major
         };
 
-        let maybe_other_str = chord_str.strip_prefix(match &chord_type {
-            ChordType::Minor => "m",
-            ChordType::Major => "M",
-            ChordType::Augmented => "aug",
-            ChordType::Diminished => "dim",
-        });
+        let extensions_str = try_remove_prefix(
+            chord_str_without_base,
+            match chord_type {
+                ChordType::Minor => "m",
+                ChordType::Major => "M",
+                ChordType::Augmented => "aug",
+                ChordType::Diminished => "dim",
+            },
+        );
 
-        let other_str = match maybe_other_str {
-            None => {
-                return Ok(ChordDetailed {
-                    base,
-                    accidental,
-                    chord_type,
-                    extension: None,
-                    additional_extension: None,
-                });
-            }
-            Some(s) => s,
-        };
-        // let a = Extension::from_str("b3");
+        if extensions_str.as_str().eq("") {
+            return Ok(ChordDetailed {
+                base,
+                accidental,
+                chord_type,
+                extension: None,
+                additional_extension: None,
+            });
+        }
 
         let mut sorted_extensions = (*Extension::VARIANTS.clone()).to_vec();
-        sorted_extensions.sort_by(|a, b| b.len().cmp(&a.len()));
-        // let first_extension_str = sorted_extensions.iter().find(|e| other_str.starts_with(*e));
-        let first_extension_str_result =
-            sorted_extensions.iter().find(|e| other_str.starts_with(*e));
+        sorted_extensions.sort_by_key(|b| std::cmp::Reverse(b.len()));
+        let first_extension_str_result = sorted_extensions
+            .iter()
+            .find(|e| extensions_str.starts_with(*e));
         if first_extension_str_result.is_none() {
             return Ok(ChordDetailed {
                 base,
@@ -120,7 +128,7 @@ impl ChordDetailed {
             _ => None,
         };
 
-        let additional_extension_str = other_str.strip_prefix(first_extension_str);
+        let additional_extension_str = extensions_str.strip_prefix(first_extension_str);
         let additional_extension: Option<Extension> = match additional_extension_str {
             Some("2") => Some(Extension::Two),
             Some("3") => Some(Extension::Three),
@@ -158,5 +166,114 @@ impl ChordDetailed {
             extension: first_extension,
             additional_extension,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[cfg(test)]
+    mod success {
+
+        mod complex_chords {
+            use std::str::FromStr;
+
+            use strum::VariantNames;
+
+            use crate::parser::types::{
+                accidental::Accidental, base::Base, chord_detailed::ChordDetailed,
+                chord_type::ChordType, extension::Extension,
+            };
+
+            #[test]
+            fn all_extension() {
+                Extension::VARIANTS.iter().for_each(|extension_str| {
+                    let mut chord_str = String::from("C");
+                    chord_str.push_str(extension_str);
+                    let extension = Extension::from_str(*extension_str);
+
+                    // FIXME: 「Cb9」が「Cbの9」なのか「Cのb9」なのか分からず、強制的に前者扱いになってしまい、failしてしまう…どうしようかな。
+                    // これって世のコード表記自体の脆弱性なのでは？かっこをつけてるのかそういうのは…
+                    // 対策案としてはテンション部分は全部かっこで囲うルールにするとか…うーんでもそしたら「C(7)」とかになっちゃうけども…いいかな？うーん。C(5)
+                    //   もしそうするなら、コードメタ情報は()で囲うのでなく[]で囲うように修正したほうがいいな。というかそれはもうやっちゃってもいいはず。紛らわしいので
+
+                    // TODO: ExtensionにM7、M9、M11、M13を忘れてたわ。追加
+                    // これも「CM9」だと「CMの9th」なのか「CのM9」なのか区別つかなくなる問題あるな。
+                    //   やっぱり前者を「CM(9)」、後者を「C（M9)」のようにかき分けるのが正義か…？なんかそう思えてきた
+                    //   というかそういう仕様で提案したら？その方がExtensionをいくらでもつけれるよね？（C(-5,9,13)とか）
+                    //   あと知恵袋に「CM9」の問題を聞いてみてもサンプリングにいいかもね
+
+                    println!("chord_str: {}", chord_str);
+                    println!("extension: {:?}", extension);
+
+                    assert_eq!(
+                        ChordDetailed::from_str(&chord_str).unwrap(),
+                        ChordDetailed {
+                            base: Base::C,
+                            accidental: None,
+                            chord_type: ChordType::Major,
+                            extension: Some(extension.unwrap()),
+                            additional_extension: None,
+                        }
+                    );
+                });
+            }
+
+            #[test]
+            fn half_diminish_raw() {
+                assert_eq!(
+                    ChordDetailed::from_str("C#m7-5").unwrap(),
+                    ChordDetailed {
+                        base: Base::C,
+                        accidental: Some(Accidental::Sharp),
+                        chord_type: ChordType::Minor,
+                        extension: Some(Extension::Seven),
+                        additional_extension: Some(Extension::FlatFive),
+                    }
+                );
+            }
+
+            #[test]
+            fn half_diminish_symbol() {
+                assert_eq!(
+                    ChordDetailed::from_str("Co11").unwrap(),
+                    ChordDetailed {
+                        base: Base::C,
+                        accidental: None,
+                        chord_type: ChordType::Major,
+                        extension: Some(Extension::HalfDiminish),
+                        additional_extension: Some(Extension::Eleven),
+                    }
+                );
+            }
+
+            #[test]
+            fn nine_add13() {
+                assert_eq!(
+                    ChordDetailed::from_str("C#m9add13").unwrap(),
+                    ChordDetailed {
+                        base: Base::C,
+                        accidental: Some(Accidental::Sharp),
+                        chord_type: ChordType::Minor,
+                        extension: Some(Extension::Nine),
+                        additional_extension: Some(Extension::Add13),
+                    }
+                );
+            }
+
+            #[test]
+            fn nine_flat13() {
+                assert_eq!(
+                    ChordDetailed::from_str("C#m9b13").unwrap(),
+                    ChordDetailed {
+                        base: Base::C,
+                        accidental: Some(Accidental::Sharp),
+                        chord_type: ChordType::Minor,
+                        extension: Some(Extension::Nine),
+                        additional_extension: Some(Extension::FlatThirteen),
+                    }
+                );
+            }
+        }
     }
 }
