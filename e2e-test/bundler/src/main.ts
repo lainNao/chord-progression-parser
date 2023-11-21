de877a04d462c5b5d6df4b3f758816001f2421bf
lainNao <k.yamada.biz.24@gmail.com>
2023-11-22 03:38:42 +0900

    (e2e-bundler) refactor display

--- e2e-test/bundler/src/main.ts
+++ e2e-test/bundler/src/main.ts
@@ -6,6 +6,35 @@ import {
 } from "../../../pkg/pkg-bundler/error_code_message_map";
 // import sampleChords from "./sample-chord-progression.txt?raw";
 
+const getHighlightedTextLines = ({
+  text,
+  row,
+  col,
+  length,
+}: {
+  text: string;
+  row: number;
+  col: number;
+  length: number;
+}): string[] => {
+  const lines = text.split("\n");
+
+  if (row < 0 || row >= lines.length || col < 0 || col >= lines[row].length) {
+    return lines; // 範囲外の場合は元のテキストをそのまま返す
+  }
+
+  const start = Math.max(col, 0);
+  const end = Math.min(col + length, lines[row].length);
+
+  const beforeHighlight = lines[row].substring(0, start);
+  const highlight = lines[row].substring(start, end);
+  const afterHighlight = lines[row].substring(end);
+
+  lines[row] = `${beforeHighlight}<mark>${highlight}</mark>${afterHighlight}`;
+
+  return lines;
+};
+
 function main() {
   const elms = {
     textarea: document.querySelector<HTMLTextAreaElement>("#textarea")!,
@@ -26,22 +55,40 @@ function main() {
       console.log(e);
       if (typeof e === "string") {
         const parsedError = JSON.parse(e);
+
+        // NOTE: This is a workaround for the case where the error is not a JSON string.
         if (typeof parsedError === "string") {
           elms.result.innerHTML = String(parsedError);
           return;
         }
 
+        // NOTE: This is a controlled error case
         if (
           typeof parsedError == "object" &&
           parsedError !== null &&
           "code" in parsedError
         ) {
-          const errorMessage = getErrorMessage({
-            errorCode: parsedError.code as ErrorCode,
-            lang: "ja",
-          });
-          elms.result.innerHTML = errorMessage ?? "Unknown error";
+          // FIXME: generate and use appropriate guard
+          if (parsedError.position) {
+            elms.result.innerHTML =
+              "" +
+              `${parsedError.position.lineNumber}行目: ` +
+              getErrorMessage({
+                errorCode: parsedError.code as ErrorCode,
+                lang: "ja",
+              }) +
+              `(${parsedError.code})\n` +
+              "\n" +
+              getHighlightedTextLines({
+                text: elms.textarea.value,
+                row: parsedError.position.lineNumber - 1,
+                col: parsedError.position.columnNumber - 1,
+                length: parsedError.position.length,
+              })[parsedError.position.lineNumber - 1];
+          }
         }
+      } else {
+        elms.result.innerHTML = "Unknown error";
       }
     }
   };
