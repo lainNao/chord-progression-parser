38b3ee1667780bfaac79a47780419c165fba4e2d
lainNao <none>
2023-11-25 17:07:09 +0900

    add snapshot test by insta and cargo-insta

--- src/lib.rs
+++ src/lib.rs
@@ -163,6 +163,31 @@ mod tests {
         }
 
         #[test]
+        fn complex_input_snapshot() {
+            let input: &str = "
+@section=Intro
+[key=E]E-C#m(7)-Bm(7)-C#(7)
+F#m(7)-Am(7)-F#(7)-B
+
+@section=Verse
+E-C#m(7)-Bm(7)-C#(7)
+F#m(7)-Am(7)-F#(7)-B
+
+@section=Chorus
+[key=C]C-C(7)-FM(7)-Fm(7)
+C-C(7)-FM(7)-Dm(7)
+Em(7)-E(7)
+        
+@section=Interlude
+C-A,B
+
+[key=C]C(M9)-CM(9)
+";
+
+            insta::assert_debug_snapshot!(parse_chord_progression_string(input));
+        }
+
+        #[test]
         fn complex_input_can_be_parsed() {
             let input: &str = "
 @section=Intro
